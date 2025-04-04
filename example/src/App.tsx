e1c1872b4cb412aa68f004678d06999f7a3090f6
devasling <devasling@gmail.com>
2025-04-04 20:28:57 +0800

    chore: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,42 @@
-import { multiply } from 'react-native-awesome-library';
-import { Text, View, StyleSheet } from 'react-native';
+import { SectionGridList } from 'react-native-awesome-library';
+import { Text, View, StyleSheet, type SectionListData } from 'react-native';
 
-const result = multiply(3, 7);
+const SECTIONS_DATA: SectionListData<string, { title: string }>[] = [
+  {
+    title: 'Title 1',
+    data: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+  },
+  {
+    title: 'Title 2',
+    data: ['Item 1', 'Item 2'],
+  },
+  {
+    title: 'Title 3',
+    data: ['Item 1'],
+  },
+  {
+    title: 'Title 4',
+    data: ['Item 1', 'Item 2'],
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <SectionGridList
+        sections={SECTIONS_DATA}
+        numColumns={3}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.itemContainer}>
+              <Text>{item}</Text>
+            </View>
+          );
+        }}
+        renderSectionHeader={(info) => <Text>{info.section.title}</Text>}
+        ItemSeparatorComponent={() => <View style={{ width: 20 }} />}
+        RowSeparatorComponent={() => <View style={{ height: 20 }} />}
+      />
     </View>
   );
 }
@@ -14,7 +44,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    paddingTop: 50,
+  },
+  itemContainer: {
+    backgroundColor: 'pink',
     justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    // width: 100,
   },
 });
